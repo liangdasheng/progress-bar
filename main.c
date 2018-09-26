870a07074937a4039c00a4d82bb9f8a1cc7c0c18
dasheng <1503312325@qq.com>
2018-09-26 08:04:53 +0800

    main.c

--- main.c
+++ main.c
@@ -3,5 +3,15 @@
 
 int main()
 {
-  char split[4]= 
+  char split[4]= {'-','/','|','\'};
+  char buf[1024] = {0};
+  int i = 0;
+  for(; i < 100; ++i)
+  {
+    buf[i] = '#';
+    printf("[%d%%][%c][%s]\r",i,split[i%4],buf);
+    fflush(stdout);
+    usleep(1000*1000);
+  }
+  return 0;
 }
